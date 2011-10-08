75a9985797dc2ff72e9e241a19335c33374c621e
Yusuke Ohshima <git@yukke.org>
2011-10-08 13:57:46 +0900

    update

--- broadcast.sh
+++ broadcast.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+#
+# broadcast.sh
+# 
+# this is script which broadcast with webcam on NicoNama, UStream and more.
+# Sorry, this is for Linix only.
+# 
+# Required tool:
+#  - Gstreamer >= 0.10
+#  - ffmpeg with librtmp
+#  - v4l2looopback 
+# 
+# Optional tool:
+#  - guvcview
+# 
+
+RTMP_URI="${1:-__cameratest__}"
+STREAM="${2:-__local__}"
+
 # --- Settings ---
 CAMERA_SOURCE="/dev/video0"
 WIDTH="640"
@@ -14,26 +32,31 @@ ASAMPLINGRATE="44100"
 ABITRATE="96"
 ACHANNEL="2"
 
-#RENDERFONT="HeadlineNEWS 16"
 TITLEFONT="Acknowledge TT BRK Regular 16"
-TITLETEXT="CR STEALTH block.III Broadcast channel"
+TITLETEXT="CR STEALTH block.III Broadcast Channel"
 TITLEPOS="halign=left deltay=0 xpad=5 ypad=5"
 CLOCKFONT="Simpleton BRK 16"
 
-if [ "${1}x" = "x" ]; then
-  OUTPUT_URI="file:///dev/null"
-else
-  RTMP_URI="$1"
-  STREAM="$2"
-  # to NicoNama Broadcast
-  OUTPUT_URI="${RTMP_URI}/${STREAM} flashver=FMLE/3.0\20(compatible;\20FMSc/1.0) swfUrl=${RTMP_URI}"
+if [ "${RTMP_URI}" != "__cameratest__" ]; then
+  if [ "${STREAM}" != "__local__" ]; then
+    # to NicoNama Broadcast
+    OUTPUT_URI="${RTMP_URI}/${STREAM} flashver=FMLE/3.0\20(compatible;\20FMSc/1.0) swfUrl=${RTMP_URI}"
+  else
+    OUTPUT_URI="${RTMP_URI}"
+  fi
 fi
 
-#----------
+HAS_GUVCVIEW="no"
+if [ "`which guvcview`" ]; then
+  HAS_GUVCVIEW="yes"
+fi
 
-lsmod | grep -q v4l2loopback || sudo modprobe v4l2loopback
+# 
+if ! lsmod | grep -q v4l2loopback; then
+   sudo modprobe v4l2loopback
+fi
 
-gst-launch -v v4l2src device=${CAMERA_SOURCE} ! \
+gst-launch v4l2src device=${CAMERA_SOURCE} ! \
            "video/x-raw-yuv,width=${WIDTH},height=${HEIGHT},bitrate=${FPS}/1" ! \
            clockoverlay halign="right" valign="top" font-desc="$CLOCKFONT" time-format="%H:%M" shaded-background=yes ! \
            textoverlay text="$TITLETEXT" font-desc="$TITLEFONT" $TITLEPOS shaded-background=yes ! \
@@ -42,12 +65,16 @@ gst-launch -v v4l2src device=${CAMERA_SOURCE} ! \
               m. ! queue ! xvimagesink &
 sleep 3
 
-guvcview -o &
+[ "$HAS_GUVCVIEW" = "yes" ] && guvcview -o &
 
-ffmpeg -v 1 -threads 0 \
-       -f video4linux2 -i ${VIDEO_SOURCE} -bt ${VBITRATE}k \
-       -f alsa -i ${AUDIO_SOURCE} -ar ${ASAMPLINGRATE} -ab ${ABITRATE}k -ac ${ACHANNEL} -async 1 \
-       -vcodec libx264 -vpre medium -x264opts "bitrate=${VBITRATE}" \
-       -acodec libmp3lame \
-       -f flv  -r ${FPS} \
-       "${OUTPUT_URI}"
+if [ "$RTMP_URI" != "__cameratest__" ]; then
+  ffmpeg -v 1 -threads 0 \
+         -f video4linux2 -i ${VIDEO_SOURCE} -bt ${VBITRATE}k \
+         -f alsa -i ${AUDIO_SOURCE} -ar ${ASAMPLINGRATE} -ab ${ABITRATE}k -ac ${ACHANNEL} -async 1 \
+         -vcodec libx264 -vpre medium -x264opts "bitrate=${VBITRATE}" \
+         -acodec libmp3lame \
+         -f flv  -r ${FPS} \
+         "${OUTPUT_URI}" &
+fi
+
+wait 
